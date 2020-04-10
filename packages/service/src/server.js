0a721a7e31f7c1a3925bed143c43220b3bb471ac
Helior Colorado <me@helior.info>
2020-04-09 21:21:32 -0700

    fix: no warnings on image builds; using ENV vars

--- packages/service/src/server.js
+++ packages/service/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello Wizards!'));
+const name = process.env.name || 'Wizards';
+
+app.get('/', (req, res) => res.send(`Hello ${name}!`));
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
