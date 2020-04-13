94b539741c4049a6b249862714d87a1ea3805822
Helior Colorado <me@helior.info>
2020-04-13 02:19:42 -0700

    feat: WebSocket usage implementation
    Helpful for me to decide how to architect the communication.

--- packages/service/src/server.js
+++ packages/service/src/server.js
@@ -1,7 +1,82 @@
 const express = require('express');
-const app = express()
-const port = 3000;
-const name = process.env.name || 'Wizards';
+const http = require('http');
+const WebSocket = require('ws');
 
-app.get('/', (req, res) => res.send(`Hello ${name}!`));
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+console.log('Running...');
+
+const app = express()
+const port = 8000;
+const name = process.env.name || 'Wizards';
+app.get('/hello', (req, res) => res.send(`Hello ${name}!`));
+// app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server started on port ${server.address().port} :)`);
+});
+
+/**
+ * WebSocket service.
+ *
+ * Links to look at for improvements:
+ * - https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
+ * - https://github.com/websockets/ws/wiki/client-auto-reconnect-with-ping-listener---exponential-back-off-timeout
+ */
+const wss = new WebSocket.Server({ server: server });
+let connectedUsers = 0;
+let users = [];
+
+wss.on('connection', function connection(ws, request) {
+  connectedUsers++;
+  // Start here.
+  console.log('—————————————————————');
+  console.log('connection', `{ws} {request}`);
+  console.log(`Connected users: ${connectedUsers}`);
+  ws.send(`Server: Connection.`);
+
+  ws.on('close', function close(code, reason) {
+    connectedUsers--;
+    console.log('close', code, reason);
+    console.log(`Connected users: ${connectedUsers}`);
+    ws.send(`Server: Close. code: {code} {reason}`);
+  });
+
+  ws.on('error', function error(error) {
+    console.log('error', error);
+    ws.send(`Server: Error {error}`);
+  });
+
+  ws.on('message', function message(data) {
+    // DON'T SEND BACK TO CLIENT, WILL CAUSE LOOP!
+    try {
+      console.log(JSON.parse(data));
+    }
+    catch (e) {
+      console.error("Catch:",e);
+    }
+  });
+
+  ws.on('open', function open() {
+    console.log('open');
+    ws.send(`Server:open`);
+  });
+
+  ws.on('ping', function ping(data) {
+    console.log('ping', data);
+    ws.send(`Server:ping {data}`);
+  });
+
+  ws.on('pong', function pong(data) {
+    console.log('pong', data);
+    ws.send(`Server:pong {data}`);
+  });
+
+  ws.on('unexpected-response', function unexpectedResponse(request, response) {
+    console.log('unexpected', request, response);
+    ws.send(`Server:unexpected {request} {response}`);
+  });
+
+  ws.on('upgrade', function upgrade(response) {
+    console.log('upgrade', response);
+    ws.send(`Server:upgrade {response}`);
+  });
+});
--- packages/web/src/slayer/app/App.js
+++ packages/web/src/slayer/app/App.js
@@ -12,10 +12,11 @@ import './App.css';
  * - ✓ check offline/online mode
  * - ✓ collect client environment context
  * - render null state
- * - connect to ws
+ * - ✓ connect to ws
  * - getting game state, render the UI
  *
  * TODOS:
+ * - clean-up where Websocket responsibilities should really live.
  *
  * @param(props.window) The Web browser's window object.
  */
@@ -39,9 +40,65 @@ function App(props) {
       <div className="Spectators">
         <p>Specators go here.</p>
         <p>{clientEnv.userAgent}</p>
+        <button onClick={closeConnection}>Close Connection</button>
       </div>
     </div>
   );
+
+  // WebSocket API: usage
+  // @TODO: config for websocket URL
+  var socket = new WebSocket("ws://localhost:8000");
+
+  // Create a unique-ish client identifier
+  const time = Date.now().toString();
+  const timeID = time.substr(time.length - 6); // last 6 digits
+  console.log(`Initialize Client(${timeID}).`);
+
+  // On interval, send test data (fixme: should be a "ping")
+  const interval = setInterval( () => {
+    const msg = {
+      data: new Date(),
+      clientID: timeID
+    };
+    console.log(msg.data);
+    socket.send(JSON.stringify(msg));
+  }, 5 * 1000);
+
+  socket.onopen = function (event) {
+    console.log('onopen', event);
+    console.log(`socket protocol:`, socket.protocol);
+    console.log(`socket readyState:`, socket.readyState);
+    console.log(`socket url:`, socket.url);
+    console.log(`socket binaryType:`, socket.binaryType);
+    console.log(`socket bufferedAmount:`, socket.bufferedAmount);
+    console.log(`socket extensions:`, socket.extensions);
+    socket.send(JSON.stringify({message: `Client(${timeID}):onopen {event}`}));
+  }
+
+  socket.onmessage = function (event) {
+    // DON'T SEND BACK TO SERVER, WILL CAUSE LOOP!
+    console.log('onmessage', event);
+  }
+
+  socket.onerror = function (event) {
+    console.log('onerror', event);
+  }
+
+  socket.onclose = function (event) {
+    console.log('onclose', event);
+
+    // Stop the test data from being sent, connection is gone.
+    clearInterval(interval);
+    if (!event.wasClean) {
+      console.log('Disconnected DIRTY!');
+    }
+  }
+
+  function closeConnection(event) {
+    socket.close();
+  }
+  // END WebSocket API: usage
+
 
   return clientEnv.onLine ? loadedUI : offlineUI;
 }
