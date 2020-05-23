6a30af282faebc96de9800b449e6a12ee9ab5198
Helior Colorado <me@helior.info>
2020-05-22 20:58:57 -0700

    temp commit

--- packages/service/src/server.js
+++ packages/service/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
+const package = require('../package.json');
 console.log('Running...');
 
 const app = express();
